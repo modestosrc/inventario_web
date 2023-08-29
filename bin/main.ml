e906f8816407165969b95269e3ec6e1abecbc9d1
Mateus Felipe da Silva <ext.mateus@gmail.com>
2023-08-28 22:02:29 -0300

    Refatoração main.ml

--- bin/main.ml
+++ bin/main.ml
@@ -1,191 +1,146 @@
 [@@@ocaml.warning "-69"]
 open Sqlite3
 
-type traje = {
-    idt: int;
-    nome: string;
-    quantidade: int;
+
+type linha = {
+    id: int;
+    data1: Data.t;
+    data2: Data.t;
 }
 
+let database = "db/inventario"
+let query_from_trajes = "SELECT id, nome, quantidade FROM trajes"
+let query_from_dancarino = "SELECT id, nome, telefone FROM dancarinos"
+let delete_from_traje = "DELETE FROM trajes WHERE id = ?1"
+let delete_from_dancarino = "DELETE FROM dancarinos WHERE id = ?1"
 
 module IntMap = Map.Make(Int)
 
-let fetch_trajes () =
-    let db = db_open "db/inventario" in
-    let select_sql = "SELECT id, nome, quantidade FROM trajes" in
-    let stmt = prepare db select_sql in
+(*Query na database
+ @return: um mapa de tipo linha, contendo todas as linhas*)
+let fetch_database query =
+    let db = db_open database in
+    let stmt = prepare db query in
     let rec loop acc =
         match step stmt with
         | Rc.ROW ->
-            let idt = match Data.to_int (column stmt 0) with
-                     | Some i -> i
-                     | None -> failwith "ID é NULL na database" in
-            let nome = match Data.to_string (column stmt 1) with
-                       | Some n -> n
-                       | None -> failwith "Nome é NULL na database" in
-            let quantidade = match Data.to_int (column stmt 2) with
-                     | Some x -> x
-                     | None -> failwith "Quantidade é NULL na database" in
-            let traje = { idt; nome; quantidade} in
-            loop (IntMap.add idt traje acc)
+            let id = match Data.to_int(column stmt 0) with
+                | Some i -> i
+                | None -> failwith "Isso não devia acontecer" in
+            let data1 = column stmt 1 in
+            let data2 = column stmt 2 in
+            let linha = { id; data1; data2} in
+            loop (IntMap.add id linha acc)
         | _ -> acc
     in
-    let results = loop IntMap.empty in
-    ignore (finalize stmt);
-    ignore (db_close db);
-    results
+    let resultado = loop IntMap.empty in
+    finalize stmt |> ignore;
+    db_close db   |> ignore;
+    resultado
 
-let render_trajes trajes_map =
-    IntMap.bindings trajes_map
-        |> List.map (fun (idt, traje) ->
+(*Cria uma string contendo uma tabela html com a lista de trajes*)
+let render_traje () =
+    let linhas_map = fetch_database query_from_trajes in
+    IntMap.bindings linhas_map
+        |> List.map (fun (id, linha) ->
             Printf.sprintf
                 {|<tr id="traje/%d">
                     <td style="text-align: center;">%s</td>
                     <td style="text-align: center;">%d</td>
                     <td><Button class="btn btn-danger" hx-delete="traje/%d">Remover</Button></td>
                 </tr>|}
-                idt traje.nome traje.quantidade idt)
+                id 
+                (match (linha.data1 |> Data.to_string) with
+                        | Some s -> s
+                        | None -> "") 
+                (match (linha.data2 |> Data.to_int) with
+                        | Some i -> i
+                        | None -> 0) 
+                id)
         |> String.concat "\n"
 
-let delete_traje id =
-    let db = db_open "db/inventario" in
-    let delete_sql = "DELETE FROM trajes WHERE id = ?1" in
-    let stmt = prepare db delete_sql in
-    bind stmt 1 (Data.INT (Int64.of_int id)) |> ignore;
-  begin
-      try 
-          if step stmt <> Rc.DONE then
-              failwith "Error deleting row from database"
-            with e ->
-                finalize stmt |> ignore;
-      raise e
-  end;
-  finalize stmt |> ignore;
-  db_close db |> ignore
-
-let header_to_int request =
-        (int_of_string request)
-
-let handle_delete_traje request =
-        delete_traje request;
-        Dream.respond ~status: `OK ""
-
-let add_traje request =
-  let%lwt form_fields = Dream.form ~csrf:false request in
-  match form_fields with
-  | `Ok fields ->
-      let db = db_open "db/inventario" in
-      let sql = "INSERT INTO trajes (nome, quantidade) VALUES (?, ?)" in
-      let stmt = prepare db sql in
-
-      let nome = List.assoc "nome" fields in
-      let quantidade = List.assoc "quantidade" fields in
-      
-      bind stmt 1 (Data.TEXT nome) |> ignore;
-      bind stmt 2 (Data.INT (Int64.of_string quantidade)) |> ignore;
-
-      if step stmt <> Rc.DONE then (
-        finalize stmt |> ignore;
-        db_close db |> ignore;
-        Dream.respond ~status:`Internal_Server_Error "Erro inserindo itens!"
-      ) else (
-        finalize stmt |> ignore;
-        db_close db |> ignore;
-        Dream.html (fetch_trajes () |> render_trajes |> Pages.tabela)
-      )
-
-  | _ ->
-      Dream.respond ~status:`Bad_Request "Falha processando formulpario!"
-
-type dancarino = {
-    idd: int;
-    nome: string;
-    telefone: string;
-}
-
-let fetch_dancarinos () =
-    let db = db_open "db/inventario" in
-    let select_sql = "SELECT id, nome, telefone FROM dancarinos" in
-    let stmt = prepare db select_sql in
-    let rec loop acc =
-        match step stmt with
-        | Rc.ROW ->
-            let idd = match Data.to_int (column stmt 0) with
-                     | Some i -> i
-                     | None -> failwith "ID é NULL na database" in
-            let nome = match Data.to_string (column stmt 1) with
-                       | Some n -> n
-                       | None -> failwith "Nome é NULL na database" in
-            let telefone = match Data.to_string (column stmt 2) with
-                     | Some x -> x
-                     | None -> failwith "Telefone é NULL na database" in
-            let dancarino = { idd; nome; telefone} in
-            loop (IntMap.add idd dancarino acc)
-        | _ -> acc
-    in
-    let results = loop IntMap.empty in
-    ignore (finalize stmt);
-    ignore (db_close db);
-    results
-
-let render_dancarinos dancarino_map =
-    IntMap.bindings dancarino_map
-        |> List.map (fun (idd, dancarino) ->
+(*Cria uma string contendo uma tabela html com a lista de dancarinos*)
+let render_dancarino () =
+    let linhas_map = fetch_database query_from_dancarino in
+    IntMap.bindings linhas_map
+        |> List.map (fun (id, linha) ->
             Printf.sprintf
                 {|<tr id="dacarino/%d">
                     <td style="text-align: center;">%s</td>
                     <td style="text-align: center;">%s</td>
                     <td><Button class="btn btn-danger" hx-delete="dancarino/%d">Remover</Button></td>
                 </tr>|}
-                idd dancarino.nome dancarino.telefone idd)
+                id 
+                (match (linha.data1 |> Data.to_string) with
+                        | Some s -> s
+                        | None -> "") 
+                (match (linha.data2 |> Data.to_string) with
+                        | Some s -> s
+                        | None -> "") 
+                id)
         |> String.concat "\n"
 
-let delete_dancarino id =
-    let db = db_open "db/inventario" in
-    let delete_sql = "DELETE FROM dancarinos WHERE id = ?1" in
-    let stmt = prepare db delete_sql in
+let delete_data id query =
+    let db = db_open database in
+    let stmt = prepare db query in
     bind stmt 1 (Data.INT (Int64.of_int id)) |> ignore;
     begin
         try 
-          if step stmt <> Rc.DONE then
-            failwith "Error deleting row from database"
+            if step stmt <> Rc.DONE then
+                failwith "Error deleting row from database"
         with e ->
-            finalize stmt |> ignore;
+        finalize stmt |> ignore;
         raise e
     end;
     finalize stmt |> ignore;
-    db_close db |> ignore
+    db_close db |> ignore;
+    Dream.respond ~status: `OK ""
 
-let handle_delete_dancarino request =
-        delete_dancarino request;
-        Dream.respond ~status: `OK ""
+let insert_into_trajes = "INSERT INTO trajes (nome, quantidade) VALUES (?, ?)"
+let insert_into_dacarinos = "INSERT INTO dancarinos (nome, telefone) VALUES (?, ?)"
 
-let add_dancarino request =
-  let%lwt form_fields = Dream.form ~csrf:false request in
-  match form_fields with
-  | `Ok fields ->
-      let db = db_open "db/inventario" in
-      let sql = "INSERT INTO dancarinos (nome, telefone) VALUES (?, ?)" in
-      let stmt = prepare db sql in
-
-      let nome = List.assoc "nome" fields in
-      let telefone = List.assoc "telefone" fields in
+let insert_data insert data1 data2 =
+    let db = db_open database in
+    let stmt = prepare db insert in
       
-      bind stmt 1 (Data.TEXT nome) |> ignore;
-      bind stmt 2 (Data.TEXT telefone) |> ignore;
+    bind stmt 1 data1 |> ignore;
+    bind stmt 2 data2 |> ignore;
 
-      if step stmt <> Rc.DONE then (
+    if step stmt <> Rc.DONE then (
         finalize stmt |> ignore;
         db_close db |> ignore;
         Dream.respond ~status:`Internal_Server_Error "Erro inserindo itens!"
-      ) else (
+    ) else (
         finalize stmt |> ignore;
         db_close db |> ignore;
-        Dream.html (fetch_dancarinos() |> render_dancarinos|> Pages.dancarinos)
-      )
+        Dream.html (render_traje () |> Pages.tabela)
+    )
 
-  | _ ->
-      Dream.respond ~status:`Bad_Request "Falha processando formulpario!"
+let add_traje request =
+    let%lwt form_fields = Dream.form ~csrf:false request in
+    match form_fields with
+    | `Ok fields ->
+        let nome = List.assoc "nome" fields in
+        let quantidade = List.assoc "quantidade" fields in
+        let data1 = (Data.TEXT nome) in
+        let data2 = (Data.INT (Int64.of_string quantidade)) in
+        insert_data insert_into_trajes data1 data2
+
+    | _ ->
+        Dream.respond ~status:`Bad_Request "Falha processando formulário!"
+
+let add_dancarino request =
+    let%lwt form_fields = Dream.form ~csrf:false request in
+    match form_fields with
+    | `Ok fields ->
+        let nome = List.assoc "nome" fields in
+        let telefone = List.assoc "telefone" fields in
+        let data1 = (Data.TEXT nome) in
+        let data2 = (Data.TEXT telefone) in
+        insert_data insert_into_dacarinos data1 data2
+
+    | _ ->
+        Dream.respond ~status:`Bad_Request "Falha processando formulário!"
 
 let () =
     Dream.run
@@ -199,16 +154,16 @@ let () =
         Dream.html (Pages.inicio));
 
     Dream.get "/tabela" (fun _ ->
-        Dream.html (fetch_trajes () |> render_trajes |> Pages.tabela));
+        Dream.html (render_traje () |> Pages.tabela));
 
     Dream.get "/dancarinos" (fun _ ->
-        Dream.html (fetch_dancarinos() |> render_dancarinos |> Pages.dancarinos));
+        Dream.html (render_dancarino () |> Pages.dancarinos));
 
     Dream.delete "/traje/:id" (fun request -> 
-        (Dream.param request "id") |> header_to_int |> handle_delete_traje);
+        delete_data ((Dream.param request "id") |> int_of_string) delete_from_traje);
 
     Dream.delete "/dancarino/:id" (fun request -> 
-        (Dream.param request "id") |> header_to_int |> handle_delete_dancarino);
+        delete_data ((Dream.param request "id") |> int_of_string) delete_from_dancarino);
 
     Dream.post "/traje" add_traje;
 
